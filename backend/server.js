b4582a9e08048ba67869f3d6b6a4c487b6302ded
Muskan Aryal <aryalmuskan17@gmail.com>
2026-03-13 20:38:18 +0545

    Initial project setup with  defining backend server, PostgreSQL connection and project structure.

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./db/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Mini Pricelist API running" });
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database error");
+  }
+});
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
