ba39ccf0670528f66cbd9d02189fa172e6f2bce6
Muskan Aryal <aryalmuskan17@gmail.com>
2026-03-14 09:01:50 +0545

    Routing fixed to pricelist page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
+import Pricelist from "./pages/Pricelist"
 
 function App() {
-  return <Login />
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/pricelist" element={<Pricelist />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/Pricelist.jsx
@@ -0,0 +1,9 @@
+function Pricelist() {
+  return (
+    <div>
+      <h1>Pricelist Page</h1>
+    </div>
+  )
+}
+
+export default Pricelist
