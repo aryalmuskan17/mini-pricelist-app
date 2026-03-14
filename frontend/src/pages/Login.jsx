fe464a2f9d8ad2cda088c9a98cc1322edd4b8578
Muskan Aryal <aryalmuskan17@gmail.com>
2026-03-14 08:57:04 +0545

    Login works with token saved now onto fixing routing.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import "../styles/login.css"
-import { getTranslations } from "../services/api"
+import { getTranslations, login } from "../services/api"
 
 function Login() {
 
   const [lang, setLang] = useState("en")
   const [texts, setTexts] = useState({})
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   useEffect(() => {
     loadTranslations()
@@ -22,13 +25,23 @@ function Login() {
     setTexts(map)
   }
 
+  const handleLogin = async () => {
+    const result = await login(email, password)
+
+    if (result.token) {
+      localStorage.setItem("token", result.token)
+      window.location.href = "/pricelist"
+    } else {
+      alert("Login failed")
+    }
+  }
+
   return (
     <div className="login-container">
 
       <div className="login-box">
 
         <div className="language-switch">
-
           <img
             src="https://storage.123fakturere.no/public/flags/GB.png"
             onClick={() => setLang("en")}
@@ -38,7 +51,6 @@ function Login() {
             src="https://storage.123fakturere.no/public/flags/SE.png"
             onClick={() => setLang("sv")}
           />
-
         </div>
 
         <img
@@ -48,11 +60,22 @@ function Login() {
 
         <h2>{texts.login_title}</h2>
 
-        <input placeholder={texts.email} />
+        <input
+          placeholder={texts.email}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <input type="password" placeholder={texts.password} />
+        <input
+          type="password"
+          placeholder={texts.password}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button>{texts.login_button}</button>
+        <button onClick={handleLogin}>
+          {texts.login_button}
+        </button>
 
       </div>
 
